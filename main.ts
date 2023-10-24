6e956e3b1926cd8ef0d10fb5fdf6be7ffda21ba2
olmes-leon <144264100+olmes-leon@users.noreply.github.com>
2023-10-24 13:03:20 -0400

    dfvg

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2023 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Mr. Leon
+ * Created on: Oct 2023
+ * This program shows if else for microbit
 */
 
 basic.showString('Hello, World!')
