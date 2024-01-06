8305475bb5271fb4c01b5b4d1d04bc84eee43060
olmes-leon <144264100+olmes-leon@users.noreply.github.com>
2024-01-06 16:16:01 -0500

    fix code

--- main.ts
+++ main.ts
@@ -1,8 +1,47 @@
 /* Copyright (c) 2023 MTHS All rights reserved
  *
- * Created by: Mr. Leon
+ * Created by: Olmes
  * Created on: Oct 2023
- * This program shows if else for microbit
+ * This program chooses two numbers and tells which one is greater or less then
 */
 
-basic.showString('Hello, World!')
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// variable
+let number1: number = randint(0, 99)
+let number2: number = randint(0, 99)
+
+// random number
+number1 = randint(0, 99)
+number2 = randint(0, 99)
+
+// 1th number
+input.onButtonPressed(Button.A, function () {
+    basic.showString('#')
+    basic.showNumber(number1)
+})
+
+// 2th number
+input.onButtonPressed(Button.B, function () {
+    basic.showString('#')
+    basic.showNumber(number2)
+})
+
+// when Shake show number1 < number2
+input.onGesture(Gesture.Shake, function () {
+    if (number1 < number2) {
+        basic.showNumber(number1)
+        basic.showString('<')
+        basic.showNumber(number2)
+        basic.showIcon(IconNames.Happy)
+
+        // on Shake show number1 > number2
+    } else {
+        basic.showNumber(number1)
+        basic.showString('>')
+        basic.showNumber(number2)
+        basic.showIcon(IconNames.Sad)
+    }
+})
