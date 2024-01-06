82da191535a3dedc3df9f1087244a7679bf898fe
olmes-leon <144264100+olmes-leon@users.noreply.github.com>
2024-01-06 16:31:24 -0500

    fixed TypeScript

--- main.ts
+++ main.ts
@@ -19,29 +19,29 @@ number2 = randint(0, 99)
 
 // 1th number
 input.onButtonPressed(Button.A, function () {
-    basic.showString('#')
-    basic.showNumber(number1)
+  basic.showString('#')
+  basic.showNumber(number1)
 })
 
 // 2th number
 input.onButtonPressed(Button.B, function () {
-    basic.showString('#')
-    basic.showNumber(number2)
+  basic.showString('#')
+  basic.showNumber(number2)
 })
 
 // when Shake show number1 < number2
 input.onGesture(Gesture.Shake, function () {
-    if (number1 < number2) {
-        basic.showNumber(number1)
-        basic.showString('<')
-        basic.showNumber(number2)
-        basic.showIcon(IconNames.Happy)
+  if (number1 < number2) {
+    basic.showNumber(number1)
+    basic.showString('<')
+    basic.showNumber(number2)
+    basic.showIcon(IconNames.Happy)
 
-        // on Shake show number1 > number2
-    } else {
-        basic.showNumber(number1)
-        basic.showString('>')
-        basic.showNumber(number2)
-        basic.showIcon(IconNames.Sad)
-    }
+    // on Shake show number1 > number2
+  } else {
+    basic.showNumber(number1)
+    basic.showString('>')
+    basic.showNumber(number2)
+    basic.showIcon(IconNames.Sad)
+  }
 })
